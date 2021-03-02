fa7fd3be3cdfee1412d38f59ff0f5dfe9462dfa2
igrekus <upload234@mail.ru>
2021-03-02 14:23:47 +0300

    WIP: add multiple return example

--- functions.v
+++ functions.v
@@ -1,6 +1,7 @@
 fn main() {
 	println('add: ${add(10, 20)}')
 	println('sub: ${sub(10, 20)}')
+	println('arithm: ${arithm(10, 20)}')
 }
 
 fn add(x int, y int) int {
@@ -9,4 +10,8 @@ fn add(x int, y int) int {
 
 fn sub(x int, y int) int {
 	return x - y
+}
+
+fn arithm(x int, y int) (int, int) {
+	return x - y, x + y
 }
