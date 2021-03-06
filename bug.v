977cf89a93998f45b3457a807152f22d95295e44
igrekus <upload234@mail.ru>
2021-03-06 17:05:32 +0300

    MISC: add two possible buggy examples to investigate

---
+++ bug.v
@@ -0,0 +1,3 @@
+nums := [1]
+println('${nums[0]'}')
+// nums2 := []int{}   // fails to compile when uncommented
---
+++ bug2.v
@@ -0,0 +1,6 @@
+var := 10   // bug is struct is not at the top of the file? (or fix docs)
+
+struct User {
+    age  int
+    name string
+}
