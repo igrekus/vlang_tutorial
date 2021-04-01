839349e1ee67e95f9fb6e25cff2f8aa71d113e82
igrekus <upload234@mail.ru>
2021-04-01 17:54:55 +0300

    WIP: define in-out params

--- with_c_code/cmod/wrapper.v
+++ with_c_code/cmod/wrapper.v
@@ -4,13 +4,17 @@ module cmod
 #flag @VROOT/c/impl.o
 #include "header.h"
 
-fn C.hello()
-
 struct C.Config{
+mut:
 	a int
 	b int
 	c int
 }
+
+fn C.hello()
+
+// int use_struct(char *str, struct Config* cfg, int* size);
+fn C.use_struct(charptr, &&C.Config, &int) int
 
 pub fn hello_from_cmod() {
 	println('hello from cmod')
@@ -21,5 +25,13 @@ pub fn use_c_wrapper_to_call_c() {
 }
 
 pub fn new_config(a int, b int, c int) C.Config {
-	return C.Config{1, 2, 3}
+	return C.Config{}
+}
+
+pub fn use_struct(s string, mut cfg C.Config, mut size &int) int {
+	size = 3
+	cfg.a = 3
+	cfg.b = 2
+	cfg.c = 1
+	return 1
 }
--- with_c_code/main.v
+++ with_c_code/main.v
@@ -6,6 +6,12 @@ fn main() {
 	// cmod.hello_from_cmod()
 	cmod.use_c_wrapper_to_call_c()
 
-	c := cmod.new_config(1, 2, 3)
+	mut c := cmod.new_config(1, 2, 3)
+	mut size := 3
+
 	println(c)
+
+	cmod.use_struct('COM1', mut c, mut &size)
+
+	println('get config and size:\n$c \n$size')
 }
